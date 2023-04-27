3566cdf363f1cab854d3dc08dc2d8ff441afa5eb
rsablina <cellovek89@gmail.com>
2023-04-27 22:24:34 +0300

    priority added

--- R/loadCountsFromH5file.R
+++ R/loadCountsFromH5file.R
@@ -3,22 +3,61 @@
 #' @import rhdf5client
 #'
 #'
-loadCountsHSDS <- function(es, src, file, sample_id = NULL, gene_id = NULL, gene_id_type = NULL) {
-  f <- HSDSFile(src, file)
-
-  if (is.null(sample_id) || is.null(gene_id) || is.null(gene_id_type)) {
-    metafilepath <- paste(dirname(file),"/newmeta.h5",sep="")
-    metaf <- HSDSFile(src, metafilepath)
-    metads <- HSDSDataset(metaf, '/meta/metatable')
+loadCountsHSDSAll <- function(es, src, dir, sample_id = NULL, gene_id = NULL, gene_id_type = NULL, priority = NULL) {
+  if (nrow(es) > 0) {
+    return(es)
   }
 
+  if (is.null(priority)) {
+    metafilepath <- paste(dir,"/meta.h5",sep="")
+    metaf <- HSDSFile(src, metafilepath)
+    priorityds <- HSDSDataset(metaf, '/priority')
+    counts_priority <- priorityds[1:priorityds@shape]
+    priority <- counts_priority[, .(directory), keyby = priority]$directory
+  }
+
+  load(paste(countsDir,"meta.rda",sep = "/"))
+  sample_amount <- DT_counts_meta[accession %in% es$geo_accession,
+                                  .(.N),
+                                  by = list(file, type_fac = factor(x = collection_type, levels = priority))]
+  if (nrow(sample_amount) == 0) {
+    return(es)
+  }
+  setorderv(x = sample_amount,cols = c("N","type_fac"),order = c(-1,1))
+  destfile <- sample_amount[,.SD[1]]$file
+
+  f <- HSDSFile(src, paste(dir, destfile, sep = "/"))
+
+  if (is.null(sample_id) || is.null(gene_id) || is.null(gene_id_type) || is.null(priority)) {
+    metafilepath <- paste(dir,"/meta.h5",sep="")
+    metaf <- HSDSFile(src, metafilepath)
+    metads <- HSDSDataset(metaf, '/meta')
+    metatable <- metads[1:metads@shape]
+    if (is.null(sample_id)) {ы
+      sample_id <- metatable$sample_id[metatable$file_name == file]
+    }
+    if (is.null(gene_id)) {
+      gene_id <- metatable$gene_id[metatable$file_name == file]
+    }
+    if (is.null(gene_id_type)) {
+      gene_id_type <- metatable$gene_id_type[metatable$file_name == file]
+    }
+    priorityds <- HSDSDataset(metaf, '/priority')
+    counts_priority <- priorityds[1:priorityds@shape]
+    priority <- counts_priority[, .(directory), keyby = priority]$directory
+  }
 
   dg <- HSDSDataset(f, gene_id)
   genes <- dg[1:dg@shape]
 
   dsamples <- HSDSDataset(f, samples_id)
-  samples <- dsamples[1:dsamples@shape]
-  samples <- dsamples[1:200000]
+  cnt <- 1
+  samples <- list()
+  while (cnt < dsamples@shape) {
+    samples <- c(samples, dsamples[cnt:min(c(cnt + 10000, dsamples@shape))])
+    cnt <- min(c(cnt + 10001, dsamples@shape))
+
+  }
 
   datasets <- listDatasets(f)
 
@@ -52,13 +91,8 @@ loadCountsHSDS <- function(es, src, file, sample_id = NULL, gene_id = NULL, gene
   return(es2)
 }
 
-loadCountsHSDSPriority <- function(es, src, file) {
-
-}
-
-getGEO <- function(name) {
 
 
-}
+
 
 
---
+++ R/updateARCHS4meta.R
@@ -0,0 +1,16 @@
+updateARCHS4meta <- function(file_path) {
+  meta <- fread(paste(file_path, "/meta.txt", sep = ""))
+  metafilename <- "meta.h5"
+  h5createFile(metafilename)
+  h5write(meta, metafilename,"meta")
+  counts_priority <- fread(paste(file_path, "/counts_priority.txt", sep = ""))
+  h5write(counts_priority, metafilename,"priority")
+  H5close()
+}
+
+updateARCHS4Index <- function(src, file_path) {
+  load(paste(file_path, "/meta.rda", sep = ""))
+
+
+
+}
